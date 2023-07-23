cabb3a649ff9e81fd28f820620be7133eb2d2bfe
darylaranha <daryl.aranha@gmail.com>
2023-07-23 02:37:46 -0400

    added dotenv file

--- Server/Config/app.js
+++ Server/Config/app.js
@@ -9,37 +9,40 @@ const morgan_1 = __importDefault(require("morgan"));
 const express_session_1 = __importDefault(require("express-session"));
 const passport_1 = __importDefault(require("passport"));
 const passport_local_1 = __importDefault(require("passport-local"));
+const passport_jwt_1 = __importDefault(require("passport-jwt"));
+let JWTStrategy = passport_jwt_1.default.Strategy;
+let ExtractJWT = passport_jwt_1.default.ExtractJwt;
 let strategy = passport_local_1.default.Strategy;
 const user_1 = __importDefault(require("../Models/user"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const db_1 = __importDefault(require("./db"));
-mongoose_1.default.connect(db_1.default.remoteURI);
-mongoose_1.default.connection.on('connected', function () {
+mongoose_1.default.connect(db_1.default.localURI);
+mongoose_1.default.connection.on("connected", function () {
     console.log(`Connected to MongoDB`);
     console.log(mongoose_1.default.connection.readyState);
 });
-mongoose_1.default.connection.on('error', function (err) {
+mongoose_1.default.connection.on("error", function (err) {
     console.log(`Error in connecting ${err}`);
 });
-mongoose_1.default.connection.on('disconnected', function () {
+mongoose_1.default.connection.on("disconnected", function () {
     console.log(`Disconnected from MongoDB`);
 });
 const index_1 = __importDefault(require("../Routes/index"));
 let app = (0, express_1.default)();
-app.use((0, morgan_1.default)('dev'));
+app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
 app.use((0, express_session_1.default)({
     secret: db_1.default.authSecret,
     saveUninitialized: false,
-    resave: false
+    resave: false,
 }));
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 passport_1.default.use(user_1.default.createStrategy());
 passport_1.default.serializeUser(user_1.default.serializeUser());
 passport_1.default.deserializeUser(user_1.default.deserializeUser());
-app.use('/api', index_1.default);
+app.use("/api", index_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -1,58 +1,67 @@
 // All imports to run the server
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
 // All imports for Authentication
-import session from 'express-session';
-import passport from 'passport';
-import passportLocal from 'passport-local';
+import session from "express-session";
+import passport from "passport";
+import passportLocal from "passport-local";
+
+// modules for jwt support
+import cors from "cors";
+import passportJWT from "passport-jwt";
+
+// define JWT aliases
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
 
 // Authentication object
 let strategy = passportLocal.Strategy;
-import User from '../Models/user';
+import User from "../Models/user";
 
 // Database modules
-import mongoose from 'mongoose';
-import db from './db';
+import mongoose from "mongoose";
+import db from "./db";
 
-mongoose.connect(db.remoteURI);
+mongoose.connect(db.localURI);
 
 // DB Connection Events
 // Event 1: When connection is established
-mongoose.connection.on('connected', function() {
-    console.log(`Connected to MongoDB`)
-    console.log(mongoose.connection.readyState)
+mongoose.connection.on("connected", function () {
+  console.log(`Connected to MongoDB`);
+  console.log(mongoose.connection.readyState);
 });
 
 // When connection has some error
-mongoose.connection.on('error', function(err) {
-    console.log(`Error in connecting ${err}`)
-})
-
-// When connection is disconnected
-mongoose.connection.on('disconnected', function () {
-    console.log(`Disconnected from MongoDB`)
+mongoose.connection.on("error", function (err) {
+  console.log(`Error in connecting ${err}`);
 });
 
+// When connection is disconnected
+mongoose.connection.on("disconnected", function () {
+  console.log(`Disconnected from MongoDB`);
+});
 
-import indexRouter from '../Routes/index';
+import indexRouter from "../Routes/index";
 
 let app = express();
 
 // All middleware modules
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express Sessions
-app.use(session({
+app.use(
+  session({
     secret: db.authSecret,
     saveUninitialized: false,
-    resave: false
-}));
+    resave: false,
+  })
+);
 
 // initialize passport
 app.use(passport.initialize());
@@ -62,9 +71,9 @@ app.use(passport.session());
 passport.use(User.createStrategy());
 
 // serialize and deserialize user data
-passport.serializeUser(User.serializeUser());
+passport.serializeUser(User.serializeUser() as any);
 passport.deserializeUser(User.deserializeUser());
 
-app.use('/api', indexRouter);
+app.use("/api", indexRouter);
 
 export default app;
